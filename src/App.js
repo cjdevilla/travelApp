6d52ed5cdcd958fcde7883189489dfd582d8c654
Camille Joy De Villa <devillacamillejoym@gmail.com>
2022-05-08 21:01:07 +0800

    add new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Destinations from './components/Destinations';
+import Search from './components/Search';
+import Selects from './components/Selects';
+import SelectsCard from './components/SelectsCard';
+import Carousel from './components/Carousel';
+
 function App() {
   return (
     <div>
         <Navbar/>
         <Hero />
+        <Destinations />
+        <Search />
+        <Selects />
+        <SelectsCard />
+        <Carousel />
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import beachVid from '../assets/beachVid.mp4'
 import {AiOutlineSearch} from 'react-icons/ai'
+
 const Hero = () => {
   return (
     <div className='w-full h-screen relative'>
@@ -11,7 +12,7 @@ const Hero = () => {
            <h2 className='py-4'>Top 1% Locations Worldwide</h2>
            <form className='flex justify-between items-center max-w-[700px] mx-auto w-full border p-1 rounded-md text-black bg-gray-100/90'>
                <div>
-               <input className='bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus-outline:none' type="text" placeholder='Search Destinations' />
+               <input className='bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none' type='text' placeholder='Search Destinations' />
                </div>
                <div>
                    <button><AiOutlineSearch size={20} className='icon'style= {{color: '#ffffff'}}/></button>
